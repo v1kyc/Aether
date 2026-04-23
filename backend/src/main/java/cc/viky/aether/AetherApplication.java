46eb9e3effee38be8002c0ac17fce3b2be259c83
v1kyc <emaniuel@proton.me>
2026-04-23 13:51:34 +0200

    feat: simple test mapping

--- backend/src/main/java/cc/viky/aether/AetherApplication.java
+++ backend/src/main/java/cc/viky/aether/AetherApplication.java
@@ -2,6 +2,8 @@ package cc.viky.aether;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 public class AetherApplication {
@@ -10,4 +12,12 @@ public class AetherApplication {
 		SpringApplication.run(AetherApplication.class, args);
 	}
 
+	@RestController
+	public class HelloController {
+
+		@GetMapping("/")
+		public String hello() {
+			return "Hello, World!";
+		}
+	}
 }
